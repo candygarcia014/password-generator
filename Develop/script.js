8f1c9d8076b0b9ba34d7aa865b0b6ee73177ab76
Candy Garcia <candygarcia014@gmail.com>
2020-11-01 18:36:53 -0800

    ccreated all vars in JS

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,28 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
+
+    generateByn.addEventListener("click", writePassword);
+
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var specialChar = "!')(*+,-.$/:;<=>%&?@[#\]^_`{|}~";
+    var num = "0123456789";
+    var newPass = []
+    var result = []
+    upperCase = upperCase.split("")
+    lowerCase = lowerCase.split("")
+    specialChar = specialChar.split("")
+    num = num.split("")
+
+    alert("Hello! Please click Generate password to begin")
+
+
+
 
 }
 
