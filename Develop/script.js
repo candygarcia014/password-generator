3ef57a2445ec3f7d90f87cb7945c9033ffc738c4
Candy Garcia <candygarcia014@gmail.com>
2020-11-01 18:54:31 -0800

    added if, else, and if else statements to JS

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,8 @@ function writePassword() {
 
     passwordText.value = password;
 
-    generateByn.addEventListener("click", writePassword);
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
 
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -23,10 +24,30 @@ function writePassword() {
 
     alert("Hello! Please click Generate password to begin")
 
+    function generatePassword() {
 
+        var pwLength = prompt("How long do you want your password to be?")
 
+    }
 
+    if (pwLength < 8 && pwLength > 128) {
+        alert("Password should be between 8 and 128 characters")
+    } else {
+        if (confirm("Do you want your password to contain letters?")) {
+            for (var i = 0; i < upperCase.length; i++) {
+                result.push(upperCase[i])
+            }
+        }
+        if (confirm("do you want your password to contain numbers?")) {
+            for (var i = 0; i < num.length; i++) {
+                result.push(num[i])
+            }
+        }
+        if (confirm("do you want your password to contain special characters?")) {
+            for (var i = 0; i < specialChar.length; i++) {
+                result.push(specialChar[i])
+            }
+
+        }
+    }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
