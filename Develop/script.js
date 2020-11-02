d79508d61dd31b7e3f0231dd25a9fec0cd28510f
Candy Garcia <candygarcia014@gmail.com>
2020-11-01 19:22:48 -0800

    promps and alerts work, but pw still not generating. ugh!

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+alert("Hello! Please click the Generate Password button below to begin")
+
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     var specialChar = "!')(*+,-.$/:;<=>%&?@[#\]^_`{|}~";
     var num = "0123456789";
-    var newPass = []
+    var passwordText = []
     var result = []
+    var pwLength = length
     upperCase = upperCase.split("")
     lowerCase = lowerCase.split("")
     specialChar = specialChar.split("")
     num = num.split("")
 
-    alert("Hello! Please click Generate password to begin")
+    passwordText.value = password;
 
     function generatePassword() {
 
-        var pwLength = prompt("How long do you want your password to be?")
+        var pwLength = prompt("How long do you want your password to be? Select a number between 8 and 128")
 
     }
 
     if (pwLength < 8 && pwLength > 128) {
         alert("Password should be between 8 and 128 characters")
-    } else {
-        if (confirm("Do you want your password to contain letters?")) {
-            for (var i = 0; i < upperCase.length; i++) {
-                result.push(upperCase[i])
-            }
-        }
-        if (confirm("do you want your password to contain numbers?")) {
-            for (var i = 0; i < num.length; i++) {
-                result.push(num[i])
-            }
-        }
-        if (confirm("do you want your password to contain special characters?")) {
-            for (var i = 0; i < specialChar.length; i++) {
-                result.push(specialChar[i])
-            }
-
+    }
+    if (confirm("Do you want your password to contain letters?")) {
+        for (var i = 0; i < upperCase.length; i++) {
+            result.push(upperCase[i])
         }
     }
+    if (confirm("do you want your password to contain numbers?")) {
+        for (var i = 0; i < num.length; i++) {
+            result.push(num[i])
+        }
+    }
+    if (confirm("do you want your password to contain special characters?")) {
+        for (var i = 0; i < specialChar.length; i++) {
+            result.push(specialChar[i])
+        }
+
+    }
 }
+
+function generatePassword() {
+    console.log(characterCatcher)
+    var passwordText = ""
+    for (var i = 0; i < pwLength; i++) {
+        passwordText += characterCatcher[Math.floor(math.random() * characterCatcher.length)]
+    }
+    return passwordText
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
